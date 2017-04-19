69c9f0219e6c80e3034acee3cb66466f9c7bb07f
Neous1 <Neous1@users.noreply.github.com>
2017-04-19 11:30:02 -0400

    Added a paddle

--- game.js
+++ game.js
@@ -1,40 +1,20 @@
 
-//            function main(){
-                // Javascript code goes here
-
-
-//                ctx.beginPath();
-//                ctx.rect(20, 40, 50, 50);
-//                ctx.fillStyle= "#FF0000";
-//                ctx.fill();
-//                ctx.closePath();
-//
-//                ctx.beginPath();
-//                ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-//                ctx.fillStyle="green";
-//                ctx.fill();
-//                ctx.closePath;
-//                
-//                ctx.beginPath();
-//                ctx.rect(160, 10, 100, 40);
-//                ctx.strokeStyle - "rgba(0, 0, 255, 0.5)";
-//                ctx.stroke();
-//                ctx.closePath();  
-
-                
-                  
-//                }
-//        
-//      document.addEventListener("DOMContentLoaded, main, false");
-        
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 1; 
-var dy = -1;
+var dx = 2; 
+var dy = -2;
 var ballRadius = 10;
+
+var rightPressed = false;
+var leftPressed = false;
 
 
 function drawBall() {
@@ -47,16 +27,55 @@ function drawBall() {
 }
 
 function draw() {
-    // drawing ball
+    
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
+    
     x += dx;
     y += dy;
+    
     if (y + dy > canvas.height-ballRadius || y + dy < ballRadius){
         dy = -dy;
     }    
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
         dx = -dx;
     }
+    
+    if(rightPressed && paddleX < canvas.width - paddleWidth){
+        paddleX +=7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
 }
+
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle= "#0095DD";
+    ctx.fill()
+    ctx.closePath();
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+            leftPressed = true;
+        }
+    }
+function keyUpHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37){
+        leftPressed = false;
+    }
+}
+
 setInterval(draw, 10);
