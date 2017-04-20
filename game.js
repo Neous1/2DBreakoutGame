62d95d4d6e7a6117d6ac2349b21007aca4ffc343
Neous1 <Neous1@users.noreply.github.com>
2017-04-20 10:44:11 -0400

    Added mouse connectivity to the canvas

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
 
-
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var paddleHeight = 10;
-var paddleWidth = 500;
+var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2; 
 var dy = -2;
+//var dx = 10; 
+//var dy = -10;
 var ballRadius = 10;
 
 var rightPressed = false;
@@ -28,10 +29,11 @@ var bricks = [];
 for (c = 0; c< brickColumnCount; c++){
     bricks[c] = [];
     for (r =0; r < brickRowCount; r++){
-        bricks[c][r] = {x: 0, y: 0};
+        bricks[c][r] = {x: 0, y: 0, status:1};
     }
 }
 
+var score = 0;
 
 function drawBall() {
     ctx.beginPath();
@@ -48,6 +50,8 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    drawScore();
+    collisionDetection()
     
     x += dx;
     y += dy;
@@ -92,20 +96,34 @@ function drawPaddle(){
 function drawBricks(){
     for(c = 0; c < brickColumnCount; c++){
         for(r = 0; r < brickRowCount; r++){
-            var brickX = (c*(brickWidth + brickPadding))+ brickOffsetLeft;
-            var brickY = (r*(brickHeight + brickPadding))+ brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(0, 0, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+            if (bricks [c][r].status ==1){
+                var brickX = (c*(brickWidth + brickPadding))+ brickOffsetLeft;
+                var brickY = (r*(brickHeight + brickPadding))+ brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();    
+            }
+            
         }
     }
 }
+
+
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e){
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width){
+        paddleX = relativeX - paddleWidth /2;
+    }
+}
 
 function keyDownHandler(e){
     if(e.keyCode == 39){
@@ -124,4 +142,33 @@ function keyUpHandler(e){
     }
 }
 
+function collisionDetection(){
+    for (c = 0; c < brickColumnCount; c++){
+        for (r = 0; r < brickRowCount; r++){
+            var b = bricks[c][r];
+            if (b.status ==1){
+                if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                    score ++;
+                    if (score == brickRowCount * brickColumnCount){
+                        alert("YOU WIN, CONGRATULATIONS \n your score is : "+score);
+                        document.location.reload();
+                    }
+                }                
+            }
+
+        }
+    }
+}
+
+function drawScore(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+    
+}
+
+
 setInterval(draw, 10);
+
